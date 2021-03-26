5cf5ec6d91fda8ba9a9030bb165fe65b49e1351a
iMoust <81176741+iMoust7@users.noreply.github.com>
2021-03-26 17:17:15 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,61 @@
+print('Bem vindo a versão 3.0 da calculadora, se gostar me chame no discord iMoust#5883')
+print('_' * 120)
+
+while True:
+    print('''
++ adição
+- subtração
+x multiplicação
+vx potência
+: divisão
+:: divisão inteira
+% resto da divisão
+''')
+
+    print('_' * 120)
+
+    numero1 = float(input('\nEscreva seu primeiro número: ').replace(',', '.'))
+    operador = input('\nSeu operador é: ')
+    numero2 = float(input('\nEscreva seu segundo número: ').replace(',', '.'))
+
+    print('_' * 120)
+
+    if operador == '+':
+        print(f'\n{numero1} + {numero2} = {numero1 + numero2}')
+
+    elif operador == '-':
+        print(f'\n{numero1} - {numero2} = {numero1 - numero2}')
+
+    elif operador == 'x':
+        print(f'\n{numero1} x {numero2} = {numero1 * numero2}')
+
+    elif operador == 'vx':
+        print(f'\n{numero1} vx {numero2} = {numero1 ** numero2}')
+
+    elif operador == ':':
+        print(f'\n{numero1} : {numero2} = {numero1 / numero2}')
+
+    elif operador == '::':
+        print(f'\n{numero1} :: {numero2} = {numero1 // numero2}')
+
+    elif operador == '%':
+        print(f'\n{numero1} % {numero2} = {numero1 % numero2}')
+
+    else:
+        print('\n\nVocê colocou o operador errado, tente novamente\n\n\n')
+
+    print('_' * 120)
+
+    pararwhile = input('\nVocê quer continuar a calcular? "S" / "N": ').upper()
+
+    if pararwhile == 'S':
+        print('\n\nObrigado por continuar')
+
+    elif pararwhile == 'N':
+        print('\n\nEspero vê-lo novamente')
+        break
+
+    else:
+        print('\n\nObrigado por continuar')
+
+    print('_' * 120)
